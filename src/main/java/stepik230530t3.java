92238127f66182e42ed9fc744f280f14ebdf977e
m-khismatullin <m.khismatullin@yandex.ru>
2023-05-31 11:37:35 +0500

    1. исправил ошибку с выводом строки из пробелов (.limit теперь действительно ограничивается 10 элементами) 2. заменил в импортах * на конкретные с помощью "Ctrl+Alt+O" и убрал статический импорт 3. заменил строковые литералы на локальные переменные

--- src/main/java/stepik230530t3.java
+++ src/main/java/stepik230530t3.java
@@ -1,8 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.Arrays.stream;
 
 public class stepik230530t3 {
     public static void main(String[] args) {
@@ -13,16 +12,20 @@ public class stepik230530t3 {
             stringBuilder.append(scanner.nextLine());
         }
 
-        String finalString = stream(stringBuilder.toString()
+        final char spaceChar = ' ';
+        String spaceString = String.valueOf(spaceChar);
+
+        String finalString = Arrays.stream(stringBuilder.toString()
                 .toLowerCase() // без учета регистра
                 .chars() // Stream int-ов
-                .map(c -> Character.isLetterOrDigit(c) || c == 32 ? c : 32) // оставляю символы из множества буквы, цифры и пробел
+                .map(c -> Character.isLetterOrDigit(c) ? c : spaceChar) // оставляю символы из множества "буквы, цифры, пробел"
                 .mapToObj(c -> String.valueOf((char) c)) // преобразовываю в Stream строк
-                .collect(Collectors.joining()).split(" ")) // преобразовываю в строку потом в масси строк и далее в строковый Stream
+                .collect(Collectors.joining()).split(spaceString)) // преобразовываю в строку потом в масси строк и далее в строковый Stream
                 .collect(Collectors.groupingBy(e -> e, Collectors.counting())).entrySet().stream() // группирую по словам преобразовываю в Map (слово, частота) и далее в Stream Map-ов
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.<String, Long>comparingByKey())) // сортирую по значению по убыванию и потом по ключу
                 .map(e -> e.getKey()) // преобразовываю в Stream строк (оставляя только ключ)
-                .limit(11) // вывожу первых 10 слов
+                .filter(e -> e.trim().length() > 0) // удалил строки из пробелов
+                .limit(10) // вывожу первых 10 слов
                 .collect(Collectors.joining("\n")); // объединяю с разделителем "новая строка"
         System.out.println(finalString);
 
