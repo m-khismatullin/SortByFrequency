75325183cf60b98eadd49100f14ec01aa8d4602b
m-khismatullin <m.khismatullin@yandex.ru>
2023-08-23 18:30:34 +0500

    упростил код на использование регулярного выражения и печать результата сразу

--- src/main/java/stepik230530t3.java
+++ src/main/java/stepik230530t3.java
@@ -12,23 +12,21 @@ public class stepik230530t3 {
             stringBuilder.append(scanner.nextLine());
         }
 
-        final char spaceChar = ' ';
-        String spaceString = String.valueOf(spaceChar);
+        Arrays.stream(stringBuilder
+                        .toString()
+                        .toLowerCase()
+                        .replaceAll("[^\\p{L}\\p{Space}]","")
+                        .split("\\s"))
+                .collect(Collectors.groupingBy(e->e,Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.<String,Long>comparingByKey()))
+                .map(e->e.getKey())
+                .filter(e->e.trim().length()>0)
+                .limit(10)
+                .forEach(System.out::println);
 
-        String finalString = Arrays.stream(stringBuilder.toString()
-                .toLowerCase() // без учета регистра
-                .chars() // Stream int-ов
-                .map(c -> Character.isLetterOrDigit(c) ? c : spaceChar) // оставляю символы из множества "буквы, цифры, пробел"
-                .mapToObj(c -> String.valueOf((char) c)) // преобразовываю в Stream строк
-                .collect(Collectors.joining()).split(spaceString)) // преобразовываю в строку потом в масси строк и далее в строковый Stream
-                .collect(Collectors.groupingBy(e -> e, Collectors.counting())).entrySet().stream() // группирую по словам преобразовываю в Map (слово, частота) и далее в Stream Map-ов
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.<String, Long>comparingByKey())) // сортирую по значению по убыванию и потом по ключу
-                .map(e -> e.getKey()) // преобразовываю в Stream строк (оставляя только ключ)
-                .filter(e -> e.trim().length() > 0) // удалил строки из пробелов
-                .limit(10) // вывожу первых 10 слов
-                .collect(Collectors.joining("\n")); // объединяю с разделителем "новая строка"
-        System.out.println(finalString);
-
+        //Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.
     }
 
 }
